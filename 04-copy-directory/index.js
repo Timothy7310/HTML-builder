428f4cb041220f89a3d3b6e478f8265dee0cfd0d
Timothy7310 <penlist127@gmail.com>
2022-05-18 15:36:52 +0300

    feat: add copy-directory task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,28 @@
+const path = require('path');
+const fs = require('fs/promises');
 
+const createFolder = async(path) => {
+    return new Promise(() => {
+        fs.mkdir(path, {recursive: true}, (err) => {
+            if(err){
+                throw err;
+            }
+        });
+    });
+};
+
+const copyFolder = async() => {
+    return new Promise(() => {
+        fs.readdir(path.join(__dirname, 'files'), {withFileTypes: true})
+            .then((files) => {
+                files.map(file => {
+                    if (file.isFile()) {
+                        fs.copyFile(path.join(__dirname, 'files', file.name), path.join(__dirname, 'files-copy', file.name));
+                    }
+                });
+            });
+    });
+};
+
+createFolder(path.join(__dirname, 'files-copy'))
+    .then(copyFolder());
